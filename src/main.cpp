e7297397767b49c38da97b54de9f0db5c52212dc
emi <emilianobarg@gmail.com>
2023-10-18 12:18:12 -0300

    music

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,27 @@
 //
 #include "../lib/mainMenu.h"
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <iostream>
 
 int main() {
     sf::RenderWindow MENU(sf::VideoMode(800, 600), "Menu", sf::Style::Default);
     mainMenu mainMenu(MENU.getSize().x, MENU.getSize().y);
+    //Background
+    sf::RectangleShape background(sf::Vector2f(800.0f, 600.0f));
+    sf::Texture mainTexture;
+    mainTexture.loadFromFile("../background/demonwpp.jpg");
+    background.setTexture(&mainTexture);
+    //Music
+    sf::Music music;
+    if (!music.openFromFile("../music/doom.wav")) {
+        // Handle the case where the file failed to load
+        // Print an error message, log, or exit the program gracefully.
+        std::cerr << "Failed to load music file!" << std::endl;
+        return 1; // Exit the program with an error code
+    }
+
+// Rest of your code
 
 
 
@@ -66,6 +82,8 @@ int main() {
                 }
             }
             MENU.clear();
+            MENU.draw(background);
+            music.play();
             mainMenu.draw(MENU);
             MENU.display();
         }
