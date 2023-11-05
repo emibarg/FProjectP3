418e18d90b52a1ec49640d42f044acf1b3561888
Nicolás Gómez <129464523+Naiko2004@users.noreply.github.com>
2023-11-05 23:41:47 +0000

    DEV-NICO -> DEV

--- lib/Gestor.h
+++ lib/Gestor.h
@@ -30,6 +30,46 @@ public:
     Gestor(std::string ubicacion_archivo);
     void leerArchivo();
 
+
+    std::string reemplazarN(std::string base);
+
+    bool isValidUTF8(const std::string &input) {
+        for (size_t i = 0; i < input.length(); ) {
+            unsigned char current = input[i];
+            size_t bytesToRead = 0;
+
+            // Determine how many bytes the current character should be encoded with
+            if (current <= 0x7F) {
+                bytesToRead = 1;
+            } else if ((current & 0xE0) == 0xC0) {
+                bytesToRead = 2;
+            } else if ((current & 0xF0) == 0xE0) {
+                bytesToRead = 3;
+            } else if ((current & 0xF8) == 0xF0) {
+                bytesToRead = 4;
+            } else {
+                return false; // Invalid UTF-8 sequence
+            }
+
+            // Check if there are enough bytes left in the string
+            if (i + bytesToRead > input.length()) {
+                return false; // Incomplete UTF-8 sequence
+            }
+
+            // Verify that the following bytes are valid continuations
+            for (size_t j = 1; j < bytesToRead; ++j) {
+                if ((input[i + j] & 0xC0) != 0x80) {
+                    return false; // Invalid UTF-8 sequence
+                }
+            }
+
+            i += bytesToRead;
+        }
+
+        return true;
+    }
+
+
     std::vector<Producto> min_stock(int n);
     std::vector<Producto> min_stock(int n, int deposito);
     int stock(std::string nombre_articulo);
